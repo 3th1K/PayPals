7d2399f862281657408da10e9ff5c884f29828c5
MEHEDI RAHAMAN <mehedirahaman28@gmail.com>
2023-09-04 23:06:28 +0530

    Get Expense Details

---
+++ Backend/ExpenseService.Api/Handlers/GetExpenseDetailsByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using ExpenseService.Api.Exceptions;
+using ExpenseService.Api.Interfaces;
+using ExpenseService.Api.Models;
+using ExpenseService.Api.Queries;
+using MediatR;
+
+namespace ExpenseService.Api.Handlers
+{
+    public class GetExpenseDetailsByIdQueryHandler : IRequestHandler<GetExpenseDetailsByIdQuery, Expense>
+    {
+        private readonly IExpenseRepository _expenseRepository;
+        public GetExpenseDetailsByIdQueryHandler(IExpenseRepository expenseRepository)
+        {
+            _expenseRepository = expenseRepository;   
+        }
+        public async Task<Expense> Handle(GetExpenseDetailsByIdQuery request, CancellationToken cancellationToken)
+        {
+            var expense = await _expenseRepository.GetExpenseDetails(request.Id)?? throw new ExpenseNotFoundException();
+            return expense;
+        }
+    }
+}
