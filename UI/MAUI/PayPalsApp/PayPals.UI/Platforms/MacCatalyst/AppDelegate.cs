15b97afc81355c387ba4f4c91f14c947d14c67c0
MEHEDI RAHAMAN <mehedirahaman28@gmail.com>
2023-10-04 22:08:36 +0530

    adding MAUI app

---
+++ UI/MAUI/PayPalsApp/PayPals.UI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PayPals.UI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
